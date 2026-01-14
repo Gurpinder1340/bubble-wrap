115af817238912f189cb09969bad3e7d23bd7a29
Gurpinder Gill <gill.gurpinder@student.greenriver.edu>
2026-01-14 22:31:08 +0000

    Add A button functionality to change background color

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,20 @@
-#include <bn_core.h>
-#include <bn_backdrop.h>
 #include <bn_color.h>
+#include <bn_backdrop.h>
+#include <bn_core.h>
+#include <bn_keypad.h>
 
-int main() {
-    bn::core::init();                      
-    bn::backdrop::set_color(bn::color(20, 20, 31));   
+int main()
+{   
+    bn::core::init();
+    bn::backdrop::set_color(bn::color(20, 20, 31));
 
-    while(true) {
-        bn::core::update();              
+    while (true)
+    {
+
+        if (bn::keypad::a_pressed())
+        {
+            bn::backdrop::set_color(bn::color(31, 21, 22));
+        }
+        bn::core::update(); 
     }
-
 }
