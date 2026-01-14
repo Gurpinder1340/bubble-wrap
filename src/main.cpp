2c96600bc74936e85254821ac87601437858a5cf
Gurpinder Gill <gill.gurpinder@student.greenriver.edu>
2026-01-14 22:48:25 +0000

    Added B button to change background color to blue

--- src/main.cpp
+++ src/main.cpp
@@ -6,15 +6,22 @@
 int main()
 {   
     bn::core::init();
-    bn::backdrop::set_color(bn::color(20, 20, 31));
+    bn::backdrop::set_color(bn::color(20, 20, 31));  
 
     while (true)
     {
-
+        // A button (X in mGBA) 
         if (bn::keypad::a_pressed())
+        {  
+            bn::backdrop::set_color(bn::color(31, 0, 0)); // red
+        } 
+
+        // B button (Z in mGBA)
+        if (bn::keypad::b_pressed())
         {
-            bn::backdrop::set_color(bn::color(31, 21, 22));
+            bn::backdrop::set_color(bn::color(0, 0, 31)); // blue
         }
+
         bn::core::update(); 
     }
 }
