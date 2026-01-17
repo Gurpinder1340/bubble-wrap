2b15071591149042a6e9ce7d65be153f8c848969
Gurpinder Gill <gill.gurpinder@student.greenriver.edu>
2026-01-17 23:50:59 +0000

    Added lines of sprites

--- src/main.cpp
+++ src/main.cpp
@@ -8,12 +8,24 @@
 int main()
 {
     bn::core::init();
-    bn::sprite_ptr eyeL = bn::sprite_items::dot.create_sprite(-20, -20);
-    bn::sprite_ptr eyeR = bn::sprite_items::dot.create_sprite(20, -20);
+    // bn::sprite_ptr eyeL = bn::sprite_items::dot.create_sprite(-20, -20);
+    // bn::sprite_ptr eyeR = bn::sprite_items::dot.create_sprite(20, -20);
 
-    bn::sprite_ptr mouth1 = bn::sprite_items::dot.create_sprite(-20, 20);
-    bn::sprite_ptr mouth2 = bn::sprite_items::dot.create_sprite(0, 25);
-    bn::backdrop::set_color(bn::color(20, 20, 31));
+    // bn::sprite_ptr mouth1 = bn::sprite_items::dot.create_sprite(-20, 20);
+    // bn::sprite_ptr mouth2 = bn::sprite_items::dot.create_sprite(0, 25);
+    // bn::backdrop::set_color(bn::color(20, 20, 31));
+
+
+bn::sprite_ptr myCircle = bn::sprite_items::dot.create_sprite(-40, 40);
+bn::sprite_ptr myCircle2 = bn::sprite_items::dot.create_sprite(-30, 40);
+bn::sprite_ptr myCircle3 = bn::sprite_items::dot.create_sprite(-20, 40);
+bn::sprite_ptr myCircle4 = bn::sprite_items::dot.create_sprite(-10, 40);
+bn::sprite_ptr myCircle5 = bn::sprite_items::dot.create_sprite(0, 40);
+bn::sprite_ptr myCircle6 = bn::sprite_items::dot.create_sprite(10, 40);
+bn::sprite_ptr myCircle7 = bn::sprite_items::dot.create_sprite(20, 40);
+bn::sprite_ptr myCircle8 = bn::sprite_items::dot.create_sprite(30, 40);
+bn::sprite_ptr myCircle9 = bn::sprite_items::dot.create_sprite(40, 40);
+
 
     while (true)
     {
