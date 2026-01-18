458e090ac6c3661acf7a44938e6d717c8089aa12
Gurpinder Gill <gill.gurpinder@student.greenriver.edu>
2026-01-18 00:18:58 +0000

    Added row of dots

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,5 @@
+#include <bn_vector.h>
+#include <bn_log.h>
 #include <bn_sprite_ptr.h>
 #include <bn_sprite_items_dot.h>
 #include <bn_color.h>
@@ -8,6 +10,7 @@
 int main()
 {
     bn::core::init();
+    bn::vector<bn::sprite_ptr, 10> circles = {};
     // bn::sprite_ptr eyeL = bn::sprite_items::dot.create_sprite(-20, -20);
     // bn::sprite_ptr eyeR = bn::sprite_items::dot.create_sprite(20, -20);
 
@@ -16,15 +19,24 @@ int main()
     // bn::backdrop::set_color(bn::color(20, 20, 31));
 
 
-bn::sprite_ptr myCircle = bn::sprite_items::dot.create_sprite(-40, 40);
-bn::sprite_ptr myCircle2 = bn::sprite_items::dot.create_sprite(-30, 40);
-bn::sprite_ptr myCircle3 = bn::sprite_items::dot.create_sprite(-20, 40);
-bn::sprite_ptr myCircle4 = bn::sprite_items::dot.create_sprite(-10, 40);
-bn::sprite_ptr myCircle5 = bn::sprite_items::dot.create_sprite(0, 40);
-bn::sprite_ptr myCircle6 = bn::sprite_items::dot.create_sprite(10, 40);
-bn::sprite_ptr myCircle7 = bn::sprite_items::dot.create_sprite(20, 40);
-bn::sprite_ptr myCircle8 = bn::sprite_items::dot.create_sprite(30, 40);
-bn::sprite_ptr myCircle9 = bn::sprite_items::dot.create_sprite(40, 40);
+//bn::sprite_ptr myCircle = bn::sprite_items::dot.create_sprite(-40, 40);
+//bn::sprite_ptr myCircle2 = bn::sprite_items::dot.create_sprite(-30, 40);
+//bn::sprite_ptr myCircle3 = bn::sprite_items::dot.create_sprite(-20, 40);
+//bn::sprite_ptr myCircle4 = bn::sprite_items::dot.create_sprite(-10, 40);
+//bn::sprite_ptr myCircle5 = bn::sprite_items::dot.create_sprite(0, 40);
+//bn::sprite_ptr myCircle6 = bn::sprite_items::dot.create_sprite(10, 40);
+//bn::sprite_ptr myCircle7 = bn::sprite_items::dot.create_sprite(20, 40);
+//bn::sprite_ptr myCircle8 = bn::sprite_items::dot.create_sprite(30, 40);
+//bn::sprite_ptr myCircle9 = bn::sprite_items::dot.create_sprite(40, 40);
+
+for(int x = -40; x <= 40; x += 10) {
+    BN_LOG("x value", x);
+    bn::sprite_ptr myCircle = bn::sprite_items::dot.create_sprite(x, 40);
+
+ 
+    circles.push_back(myCircle);
+}
+
 
 
     while (true)
